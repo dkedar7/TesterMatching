c653905cd10f5d2ea64af73c66b82f3a6b86ec4d
Kedar Dabhadkar <dkedar@cmu.edu>
2019-02-11 19:03:09 -0500

    Update RunApp.sh

--- RunApp.sh
+++ RunApp.sh
@@ -1,5 +1,9 @@
 python -m pip install virtualenv
+
 python -m virtualenv TesterMatchingApp
+
 source TesterMatchingApp/bin/activate
+
 python -m pip install -r requirements.txt
+
 python -m flask run
